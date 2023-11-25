35075001f0fb9636cb9da636772e18ecb1a43736
ciwwan1980 <ajilqado@gmail.com>
2023-11-25 23:33:58 +0100

    updating

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
+  const history = useHistory();
+
+  const startQuiz = () => {
+    history.push('./quiz/10/18/medium/multiple');
+  };
+
   return (
     <div className="homeContainer">
       <h1>Welcome to the Quiz!</h1>
-      <Link to="/quiz/10/18/medium/multiple" className="startQuizLink">
+      <button onClick={startQuiz} className="startQuizLink">
         Start Quiz
-      </Link>
+      </button>
     </div>
   );
 };
